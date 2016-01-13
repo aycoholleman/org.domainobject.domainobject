8537fe2eda9442ecd716d8a05e3a6233945b7330
Ayco Holleman <ayco.holleman@naturalis.nl>
2016-01-13 09:32:19 +0100

    ok

--- src/main/java/org/domainobject/orm/map/LowerCaseMappingAlgorithm.java
+++ src/main/java/org/domainobject/orm/map/LowerCaseMappingAlgorithm.java
@@ -1,86 +1,63 @@
 package org.domainobject.orm.map;
 
-import org.domainobject.orm.core.Entity;
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
 
 /**
- * The LowerCaseMappingAlgorithm maps field names to column names according to common
- * naming conventions. Java properties are assumed to be camelCase, while column
- * names are assumed to be lower_case_with_underscores. Examples:
- * 
- * <pre>
- * lastName -> last_name
- * isUpperCase -> is_upper_case
- * angryManWithABadPTSTSyndrom -> angry_man_with_a_bad_ptst_syndrom
- * </pre>
- * 
- * Note that in some cases, this mapper will not work correctly with the article
- * "a" and other one-letter words. For example
- * <code>angryManWithAPTSTSyndrom</code> maps to
- * <code>angry_man_with_aptst_syndrom</code>, not to
- * </code>angry_man_with_a_ptst_syndrom</code> as you would probably hope.
- * 
- * The same logic used to map properties to columns is used to map class names
- * to {@link Entity}s. E.g. class LowerCaseMappingAlgorithm would map to table/view/query
- * lower_case_mapper.
- * 
- * @see UpperCaseMapper
+ * <p>
+ * Maps class names and field names to lowercase table names and column names,
+ * using the underscore character to separate the name parts. Examples:
+ * <ul>
+ * <li>lastName &#8594; last_name
+ * <li>isUpperCase &#8594; is_upper_case
+ * <li>patientWithASeverePTSTSyndrom &#8594; patient_with_a_severe_ptst_syndrom
+ * </ul>
+ * <p>
+ * Note that with one-letter words like the article &#34;a&#34; this mapper will
+ * not produce the likely intended result:
+ * <ul>
+ * <li>patientWithAPTSTSyndrom &#8594; patient_with_aptst_syndrom
+ * </ul>
+ * <p>
+ * The logic used to map fields to columns is also used to map class names to
+ * table names.
  */
-public class LowerCaseMappingAlgorithm extends BasicMappingAlgorithm {
+public class LowerCaseMappingAlgorithm extends AbstractMappingAlgorithm {
 
 	@Override
 	protected String mapFieldNameToColumnName(String fieldName)
 	{
-
 		char[] chars = fieldName.toCharArray();
-
-		// The length of the column can never exceed 1.5 times the length
-		// of the property. Only when the letters in the field name
-		// strictly alternate between uppercase and lowercase letters will
-		// this maximum be reached. For example "AbCdEfGh" maps to
-		// "ab_cd_fg_gh"; for every two letters there is one underscore.
-		int maxColumnLength = (int) Math.ceil((float) chars.length * 1.5F);
-		char[] column = new char[maxColumnLength];
-
-		column[0] = isUpper(chars[0]) ? toLower(chars[0]) : chars[0];
-
+		/*
+		 * Length of column name can never exceed 1.5 times the length of the
+		 * field name. Only when the letters in the field name strictly
+		 * alternate between uppercase and lowercase letters will this maximum
+		 * be reached. For example "AbCdEfGh" maps to "ab_cd_fg_gh".
+		 */
+		int maxLen = (int) Math.ceil(chars.length * 1.5F);
+		char[] colName = new char[maxLen];
+		colName[0] = isUpperCase(chars[0]) ? toLowerCase(chars[0]) : chars[0];
 		int j = 1;
 		for (int i = 1; i < chars.length; ++i) {
-			if (isUpper(chars[i])) {
-				if ((i != (chars.length - 1)) && isLower(chars[i + 1])) {
-					column[j++] = '_';
-					column[j++] = toLower(chars[i]);
+			if (isUpperCase(chars[i])) {
+				if ((i != (chars.length - 1)) && isLowerCase(chars[i + 1])) {
+					colName[j++] = '_';
+					colName[j++] = toLowerCase(chars[i]);
 				}
-				else if (isLower(chars[i - 1])) {
-					column[j++] = '_';
-					column[j++] = toLower(chars[i]);
+				else if (isLowerCase(chars[i - 1])) {
+					colName[j++] = '_';
+					colName[j++] = toLowerCase(chars[i]);
 				}
 				else {
-					column[j++] = toLower(chars[i]);
+					colName[j++] = toLowerCase(chars[i]);
 				}
 			}
 			else {
-				column[j++] = chars[i];
+				colName[j++] = chars[i];
 			}
 		}
-		return new String(column, 0, j);
-	}
-
-
-	private static boolean isUpper(char c)
-	{
-		return Character.isUpperCase(c);
-	}
-
-
-	private static boolean isLower(char c)
-	{
-		return Character.isLowerCase(c);
-	}
-
-
-	public static char toLower(char c)
-	{
-		return Character.toLowerCase(c);
+		return new String(colName, 0, j);
 	}
 
 }
--- src/main/java/org/domainobject/orm/map/UpperCaseMappingAlgorithm.java
+++ src/main/java/org/domainobject/orm/map/UpperCaseMappingAlgorithm.java
@@ -1,60 +1,47 @@
 package org.domainobject.orm.map;
 
-public class UpperCaseMappingAlgorithm extends BasicMappingAlgorithm {
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toUpperCase;
 
+/**
+ * Maps class names and field names to uppercase table names and column names,
+ * using the underscore character to separate the name parts.
+ * 
+ * @see LowerCaseMappingAlgorithm
+ * 
+ * @author Ayco Holleman
+ *
+ */
+public class UpperCaseMappingAlgorithm extends AbstractMappingAlgorithm {
+
+	@Override
 	protected String mapFieldNameToColumnName(String fieldName)
 	{
-
 		char[] chars = fieldName.toCharArray();
-
-		// The length of the column can never exceed 1.5 times the length
-		// of the property. Only when the letters in the property name
-		// strictly alternate between uppercase and lowercase letters will
-		// this maximum be reached. For example "AbCdEfGh" maps to
-		// "AB_CD_EF_GH"; for every two letters there is one underscore.
-		int maxColumnLength = (int) Math.ceil((float) chars.length * 1.5F);
-		char[] column = new char[maxColumnLength];
-
-		column[0] = toUpper(chars[0]);
-
+		int maxLen = (int) Math.ceil(chars.length * 1.5F);
+		char[] colName = new char[maxLen];
+		colName[0] = toUpperCase(chars[0]);
 		int j = 1;
 		for (int i = 1; i < chars.length; ++i) {
-			if (isUpper(chars[i])) {
-				if ((i != (chars.length - 1)) && isLower(chars[i + 1])) {
-					column[j++] = '_';
-					column[j++] = chars[i];
+			if (isUpperCase(chars[i])) {
+				if ((i != (chars.length - 1)) && isLowerCase(chars[i + 1])) {
+					colName[j++] = '_';
+					colName[j++] = chars[i];
 				}
-				else if (isLower(chars[i - 1])) {
-					column[j++] = '_';
-					column[j++] = toUpper(chars[i]);
+				else if (isLowerCase(chars[i - 1])) {
+					colName[j++] = '_';
+					colName[j++] = toUpperCase(chars[i]);
 				}
 				else {
-					column[j++] = toUpper(chars[i]);
+					colName[j++] = toUpperCase(chars[i]);
 				}
 			}
 			else {
-				column[j++] = toUpper(chars[i]);
+				colName[j++] = toUpperCase(chars[i]);
 			}
 		}
-		return new String(column, 0, j);
-
+		return new String(colName, 0, j);
 	}
 
-
-	private static boolean isUpper(char c)
-	{
-		return Character.isUpperCase(c);
-	}
-
-
-	private static boolean isLower(char c)
-	{
-		return Character.isLowerCase(c);
-	}
-
-
-	public static char toUpper(char c)
-	{
-		return Character.toUpperCase(c);
-	}
 }
